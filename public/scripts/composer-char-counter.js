952748008e6b622730e707ebd76419de84d2db7c
vshibukawa <viviane.shibukawa@gmail.com>
2019-04-16 17:04:29 +0000

    counter fixef

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,11 +3,16 @@ $(document).ready(function() {
   const countValue = Number($('.counter').text());
 
   $('.new-tweet textarea').on('input', function(e){
-    const newValue = countValue - this.textLength
-    $('.counter').text(newValue);
+    const newValue = countValue - this.textLength;
+    var colour = '#244751';
+
+    $('.counter', $(this).parent()).text(newValue);
+
     if(newValue < 0){
-      $('.counter').css('color', 'red');
+      colour = 'red';
     }
+
+    $('.counter', $(this).parent()).css('color', colour);
   });
 
 
