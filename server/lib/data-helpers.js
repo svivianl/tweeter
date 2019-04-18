983a2d5b0586a424c0fb4f78df3f62a2be9b42e5
vshibukawa <viviane.shibukawa@gmail.com>
2019-04-18 15:51:49 +0000

    refactor 1

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,20 +9,23 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, newTweet);
-        // callback(null, true);
+      db.collection("tweets").insertOne(newTweet, (err, res) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, res.ops[0]);
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
         const sortNewestFirst = (a, b) => b.created_at - a.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, tweets.sort(sortNewestFirst));
       });
     }
-
   };
 }
